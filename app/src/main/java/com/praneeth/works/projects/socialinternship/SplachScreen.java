7b579f2cc5675c5af48a07b2f6db492a23c0845b
PraneethKulukuri26 <praneethkulukuri@gmail.com>
2024-12-13 23:37:10 +0530

    completed splach screen

--- app/src/main/java/com/praneeth/works/projects/socialinternship/SplachScreen.java
+++ app/src/main/java/com/praneeth/works/projects/socialinternship/SplachScreen.java
@@ -3,6 +3,7 @@ package com.praneeth.works.projects.socialinternship;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplachScreen extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class SplachScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splach_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        },3000);
     }
 }
