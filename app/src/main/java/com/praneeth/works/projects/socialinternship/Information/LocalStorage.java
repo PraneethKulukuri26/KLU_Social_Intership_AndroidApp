e543df25847e52cfec8de6ccfbf9261eae8fefec
PraneethKulukuri26 <praneethkulukuri@gmail.com>
2024-12-13 23:44:51 +0530

    Configured local storage

--- app/src/main/java/com/praneeth/works/projects/socialinternship/Information/LocalStorage.java
+++ app/src/main/java/com/praneeth/works/projects/socialinternship/Information/LocalStorage.java
@@ -1,2 +1,12 @@
-package com.praneeth.works.projects.socialinternship.Information;public class LocalStorage {
+package com.praneeth.works.projects.socialinternship.Information;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class LocalStorage {
+    private SharedPreferences sharedPreferences;
+
+    public LocalStorage(Context context){
+        sharedPreferences=context.getSharedPreferences("Store_SAC_Social_intership_Data",Context.MODE_PRIVATE);
+    }
 }
